1c512b621e27e215ba6821b6578a7d2d44c1de09
Sean <sglover@ca.ibm.com>
2022-09-12 10:31:01 -0400

    initial

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  // $( "#checkComplete" ).click();
+});
+function regexMatch(input, pattern) {
+  let text = input.toString();
+  return text.match(pattern);
+}
+function roundHalf(x) {
+    return Math.ceil(x/.5)*.5;
+}
+function roundNumber(num, scale) {
+  if(!("" + num).includes("e")) {
+    return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+  } else {
+    var arr = ("" + num).split("e");
+    var sig = ""
+    if(+arr[1] + scale > 0) {
+      sig = "+";
+    }
+    return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+  }
+}
+function countStars(element) {
+  var children = element.children;
+  var starCount = 0;
+  for (var i = 0; i < children.length; i++) {
+    var starChild = children[i];
+    var isStar = starChild.getAttribute('alt')=='fill';
+    if(isStar){starCount++};
+  }
+  return starCount;
+}
