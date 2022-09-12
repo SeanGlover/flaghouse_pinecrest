a0b0e08f12f8e01a7283ccbdab896093d256d807
Sean <sglover@ca.ibm.com>
2022-09-12 11:21:34 -0400

    stars

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
 $(document).ready(function(){
-  // $( "#checkComplete" ).click();
+  const starElements = document.getElementsByTagName('img');
+  for (const element of starElements) {
+    document.addEventListener('mouseenter', starEntered);
+}
 });
+function starEntered(e) {
+  StarEnter(e.screenX, e.screenY);
+}
+function StarEnter(x, y) {
+    var elem = document.elementFromPoint(x, y)
+    if (elem != null)
+    {
+        var elemSz = elem.getBoundingClientRect();
+        var starId = elem.id;
+        var starIndex = parseInt(starId.substring(0, 1));
+        for (var i = 1; i <= 5; i++)
+        {
+            var stars = `#${i}${starId.substring(1, starId.length)}`;
+            $(stars).attr("src", "/images/starEmpty.png");
+        }
+        for (var i = 1; i < starIndex; i++)
+        {
+            var stars = `#${i}${starId.substring(1, starId.length)}`;
+            $(stars).attr("src", `/images/starFill.png`);
+        }
+        var lastStar = `#${starIndex}${starId.substring(1, starId.length)}`;
+        var halfFull = x < elemSz.left + elemSz.width / 2 ? `Half` : `Fill`;
+        $(lastStar).attr("src", `/images/star${halfFull}.png`);
+
+        // all stars
+        var images = document.getElementsByTagName('img');
+        if (images.length > 0)
+        {
+            var fillPoints = 0;
+            var allPoints = 0;
+            var bigStars = new Array();
+            for (var i = 0; i <= images.length; i++)
+            {
+                var img = images.item(i);
+                if (img != null)
+                {
+                    var starSz = img.getBoundingClientRect();
+                    if (starSz.width == 16) {
+                        allPoints++;
+                        fillPoints += img.src.includes('Fill') ? 1 : img.src.includes('Half') ? .5 : 0;
+                    } else { bigStars.push(img); }
+                }
+            }
+            allPoints = (allPoints == 0 ? 1 : allPoints);
+            var avgStars = bigStars.length * (fillPoints / allPoints);
+            var fullStars = Math.floor(avgStars);
+            //console.log(`Big=${bigStars.length}, Fill=${fillPoints}, All=${allPoints}, Avg=${avgStars}, Full=${fullStars}`);
+            for (var i = 0; i < bigStars.length; i++)
+            {
+                var bigStar = bigStars[i];
+                if (bigStar != null) { bigStar.src = `/images/starEmpty.png`; }
+            }
+            for (var i = 0; i < fullStars; i++)
+            {
+                var bigStar = bigStars[i];
+                if (bigStar != null) {bigStar.src = `/images/starFill.png`; }
+            }
+            if (avgStars - fullStars >= .5) { bigStars[fullStars].src = `/images/starHalf.png`; }
+        }
+    }
+    return null;
+}
 function regexMatch(input, pattern) {
   let text = input.toString();
   return text.match(pattern);
